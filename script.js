686fde19e95e24539d992232560c7256b9391788
Janis Aravena <janisaravenaespinoza@gmail.com>
2024-01-23 17:23:43 -0300

    primeros avances

---
+++ script.js
@@ -0,0 +1,7 @@
+// Función para mostrar la alerta
+function mostrarAlerta() {
+    alert('¡Hola! No olvide tomar aguita');
+}
+
+// Asocia la función al evento click del botón
+document.getElementById('mostrarAlertaBtn').addEventListener('click', mostrarAlerta);
